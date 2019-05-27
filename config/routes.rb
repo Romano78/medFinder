5f3433a6b5feb9082c4797e4fff0a31ec63b2a4f
Romain Penaruiz <rom.penaruiz@gmail.com>
2019-05-27 14:19:13 -0400

    created the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :medications, only: [:index] do
+    resources :comparisons, only: [:index]
+    resource :comparison, only: [:show]
+
+    collection do
+      get :search
+    end
+  end
 end
