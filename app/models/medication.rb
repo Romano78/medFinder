cf488c746007217e90b222a4331939cef03c8f37
Romain Penaruiz <rom.penaruiz@gmail.com>
2019-05-28 13:45:52 -0400

    removed pg search gem because we do not need it

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,15 +1,4 @@
 class Medication < ApplicationRecord
   has_many :medications_ingredients
   has_many :ingredients, through: :medications_ingredients
-
-  include PgSearch
-  pg_search_scope :search_by_medication_and_ingredients,
-    # against: [:local_name, :english_name ],
-    associated_against: {
-      ingredients: [:name]
-
-    }
-    # using: {
-    #   tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    # }
 end
