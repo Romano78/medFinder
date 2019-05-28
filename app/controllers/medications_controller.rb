6b6f11510481e5c17f0c8cc03c3b1033a518a71e
Romain Penaruiz <rom.penaruiz@gmail.com>
2019-05-28 15:00:17 -0400

    algolia search working

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,11 +1,7 @@
 class MedicationsController < ApplicationController
   def index
      if params[:query].present?
-      sql_query = " \
-        medications.english_name @@ :query \
-        OR ingredients.name @@ :query \
-      "
-      @medications = Medication.joins(:ingredients).where(sql_query, query: "%#{params[:query]}%")
+      @medications = Medication.search(params[:query])
     else
       @medications = Medication.all
     end
