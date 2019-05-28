0db88e76ba308dde30f753769c253523abce613f
Claire Deacon <clairelouisebd@gmail.com>
2019-05-28 11:19:03 -0400

    index page for medication comparisons

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -1,5 +1,7 @@
 class ComparisonsController < ApplicationController
   def index
+    @medication = Medication.find(params[:medication_id])
+    @medications = Medication.where(country: params[:country])
   end
 
   def show
--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,3 +1,8 @@
 class Medication < ApplicationRecord
   has_many :ingredients
+
+  def country_name
+    c = ISO3166::Country[country]
+    c.translations[I18n.locale.to_s] || c.name
+  end
 end
