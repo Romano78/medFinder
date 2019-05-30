bf477e446c1254e3122801f5d23cb45687792fd6
Romain Penaruiz <rom.penaruiz@gmail.com>
2019-05-30 08:50:01 -0400

    removed the binding.pry

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -9,8 +9,6 @@ class MapsController < ApplicationController
     response = open("https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{ENV['GOOGLE_API_KEY']}&location=#{params[:lat]},#{params[:lng]}&rankby=distance&keyword=pharmacy").read
     results = JSON.parse(response)
 
-    binding.pry
-
     render( {json: results["results"][0]["geometry"]["location"]})
   end
 end
