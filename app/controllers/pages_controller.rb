51527d0539324ca52dadd01b184362888273cca1
AxelleBZ <axellezeller@hotmail.fr>
2019-06-03 11:50:49 -0400

    create the login,logout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, except: :dashboard
   def home
-    reset_session
+    #recommanded by victor to reset progress bar, disable beccause it logout the user
+    # reset_session
   end
 
   def mf_styles
