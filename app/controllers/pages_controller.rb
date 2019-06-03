f267bb774a59a21b896dc6d935d9fdc7aa790825
Claire Deacon <clairelouisebd@gmail.com>
2019-06-03 15:40:31 -0400

    deleted unnecessary

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, except: :dashboard
   def home
-    #recommanded by victor to reset progress bar, disable beccause it logout the user
-    # => reset_session
     session[:home_med] = nil
     session[:destination] = nil
     session[:destination_med] = nil
