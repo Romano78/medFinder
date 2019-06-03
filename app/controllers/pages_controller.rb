70c1bdabcfaf6b9e8f9a76c70ae119e85b6518a9
Claire Deacon <clairelouisebd@gmail.com>
2019-06-03 15:39:43 -0400

    fixed conflict between prog bar and login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, except: :dashboard
   def home
     #recommanded by victor to reset progress bar, disable beccause it logout the user
-    # reset_session
+    # => reset_session
+    session[:home_med] = nil
+    session[:destination] = nil
+    session[:destination_med] = nil
+    session[:query] = nil
+    session[:home_country] = nil
   end
 
   def mf_styles
