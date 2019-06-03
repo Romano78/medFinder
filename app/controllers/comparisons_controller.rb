f6156632d50c0ae91f4e55bdc1074dd3bb2a1375
AxelleBZ <axellezeller@hotmail.fr>
2019-06-03 10:42:47 -0400

    create a login/logout

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -1,4 +1,5 @@
 class ComparisonsController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
     session[:home_med] = params[:medication_id]
     session[:destination] = params[:country]
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, except: :dashboard
   def home
     reset_session
   end
 
   def mf_styles
+  end
+
+  def dashboard
   end
 end
